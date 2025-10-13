8ef8bc2a919ec76171dd887e4540a30a50c6b08a
Abdur Raqeeb <kar9790707358@gmail.com>
2025-10-13 17:17:11 +0530

    Add files via upload

---
+++ pdf_merger/main.py
@@ -0,0 +1,12 @@
+import PyPDF2
+
+pdfiles = ["sample-1.pdf", "sample-2.pdf"]
+merger = PyPDF2.PdfMerger()
+
+for filename in pdfiles:
+    pdfFile = open(filename, 'rb')
+    pdfReader = PyPDF2.PdfReader(pdfFile)
+    merger.append(pdfReader)
+
+pdfFile.close()
+merger.write("merged.pdf")
